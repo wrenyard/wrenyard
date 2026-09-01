fe66c850305e90c0ea91b6b3ef703085f1b7e0ee
Dluck <dutiesheng@outlook.com>
2026-09-01 12:57:01 +0800

    release: prepare v1.0.0-dev.18

--- apps/desktop/src/profile.ts
+++ apps/desktop/src/profile.ts
@@ -88,7 +88,7 @@ export async function prepareProfile(
 
   const manifest = {
     name: '@wrenyard/dsh-profile',
-    version: '1.0.0-dev.17',
+    version: '1.0.0-dev.18',
     private: true,
     type: 'module',
     dsh: {
